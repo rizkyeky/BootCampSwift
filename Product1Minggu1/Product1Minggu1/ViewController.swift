087f8bda49581efd947ecd02d0a66bb3eb409adb
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2023-10-25 16:24:27 +0700

    new project uikit

---
+++ Product1Minggu1/Product1Minggu1/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Product1Minggu1
+//
+//  Created by Eky on 25/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var backgroundProfile: UIView!
+    
+    @IBOutlet weak var avaProfile: UIImageView!
+    
+    @IBOutlet weak var cardList: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        backgroundProfile.layer.cornerRadius = 24
+        
+        avaProfile.clipsToBounds = true
+        avaProfile.layer.cornerRadius = avaProfile.frame.size.width / 2
+        avaProfile.layer.borderWidth = 8.0
+        avaProfile.layer.borderColor = UIColor.systemBlue.cgColor
+        
+        cardList.layer.cornerRadius = 24
+        cardList.layer.shadowColor = UIColor.systemGray.cgColor
+        cardList.layer.shadowOpacity = 0.2
+        cardList.layer.shadowOffset = CGSize(width: 2, height: 2)
+        cardList.layer.shadowRadius = 4
+    }
+}
+
---
+++ Product1Minggu1/Product1Minggu1UITests/Product1Minggu1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Product1Minggu1UITestsLaunchTests.swift
+//  Product1Minggu1UITests
+//
+//  Created by Eky on 25/10/23.
+//
+
+import XCTest
+
+final class Product1Minggu1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
